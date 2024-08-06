a376d63c5964619e511baa69a7898da209ba1762
scopor <39816569+scopor@users.noreply.github.com>
2024-08-06 14:24:24 +0800

    Update UnsplashApi.ts

--- src/api/UnsplashApi.ts
+++ src/api/UnsplashApi.ts
@@ -1,18 +1,19 @@
 import axios from 'axios';
 
 const UNSPLASH_ACCESS_KEY = process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY;
+console.log(UNSPLASH_ACCESS_KEY);
 const UNSPLASH_API_URL = 'https://api.unsplash.com/photos/random';
 
 export const fetchWallpapers = async (page: number = 1, perPage: number = 10) => {
     try {
         const randomParam = Math.random();
-        console.log(UNSPLASH_ACCESS_KEY);
+        console.log(${UNSPLASH_ACCESS_KEY});
         const response = await axios.get(UNSPLASH_API_URL, {
             headers: {
                 Authorization: `Client-ID ${UNSPLASH_ACCESS_KEY}`,
             },
             params: {
-                client_id: UNSPLASH_ACCESS_KEY,
+                client_id: ${UNSPLASH_ACCESS_KEY},
                 page: page,
                 count: perPage,
                 random: randomParam
